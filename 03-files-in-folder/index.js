390133b65a3398a89fddbb230e898fcab2b68ddb
Aleksander-true <al_seryapin@mail.ru>
2021-11-03 18:53:56 +0300

    refact: 03-files-in-folder by promises

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,7 +1,35 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const path = require('path');
 const url = path.join(__dirname,'secret-folder');
 
+readDir(url);
+
+async function readDir(url) {
+  try {
+    const files = await fs.readdir(url, {withFileTypes: true})
+    showFiles(files, url)
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+function showFiles(files, url) {
+  files.forEach( async function(file) {
+    if (file.isFile()) {
+      let extname = path.extname(file.name);
+      let fileName = path.basename(file.name, extname) 
+      try {
+        let stats = await fs.stat(path.join(url,file.name))
+        console.log(`${fileName} - ${extname.slice(1)} - ${Math.round(stats.size/1.024)/1000} kb`);
+      } catch (error) {
+        console.error(err);
+      }
+    }
+  })
+}
+
+
+/*
 fs.readdir(url, {withFileTypes: true}, (err,files) => {
   if (err) throw err;
   files.forEach( (file)=> {
@@ -18,4 +46,4 @@ fs.readdir(url, {withFileTypes: true}, (err,files) => {
   })
   });
 
-
+*/
