832917a1ae64c53fe7b720ef6aaabb2e30deaae3
Aleksander-true <al_seryapin@mail.ru>
2021-11-05 17:03:08 +0300

    refact: 05-merge-styles to read by stream

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,73 +1,45 @@
 const fs = require('fs/promises');
+const fsStream = require('fs');
 const path = require('path');
-const { nextTick } = require('process');
 const SRC_URL = path.join(__dirname,'styles');
 const DEST_URL = path.join(__dirname,'project-dist', 'bundle.css');
 
-isExistBundle(DEST_URL)
-.then((exist)=> {deleteFile(DEST_URL)},
-      (notExist) => { })
+createEmptyFile(DEST_URL)
 .then(() => readDir(SRC_URL))
-.then((files)=> readAll(SRC_URL,files))
-.then((dates) => appendToBundle(DEST_URL, dates))
-.catch((error)=> console.log('Error:', error.message));
+.then((files)=> getUrls(SRC_URL,files))
+.then((urls) => appendToBundle(DEST_URL, urls))
+.catch((error)=> console.log('Error', error.message));
 
-function isExistBundle(url) {
-  console.log('isExistBundle URL', url)
-  return fs.access(url)
-}
-
-function deleteFile(url) {
-  console.log('deleteFile URL', url)
-  return fs.rm(url)
+function createEmptyFile(url) {
+  return fs.writeFile(url,'')
 }
 
 function readDir(url) { 
-  console.log('readDir URL', url)
     return fs.readdir(url, {withFileTypes: true})
 }
 
-function readAll(url,files) {
-  console.log('readAll files', files)
-  let promises = files.map((file) => {
-    if (path.extname(file.name) === '.css') {
-      let src = path.join(url, file.name);
-      return readFile(src);
-    }
-  }).filter( item => item !== undefined)
-  return Promise.all(promises)
+function getUrls(url,files) {
+  return files.map((file) => path.extname(file.name) === '.css' ? path.join(url, file.name) : undefined)
+  .filter( item => item !== undefined)
 }
 
-async function readFile(url) {
-  console.log('readFile URL', url)
-  let data = fs.readFile(url, {encoding: 'utf8'})
-  return data
-}
+async function appendToBundle(destUrl, urlArray) {
+  appendNext(await urlArray.shift())
   
-async function appendToBundle(url, dates) {
-  console.log('appendToBundle dates', url)
-  appendNext(await dates.shift())
-
-async  function appendNext(data) {
-    if (data) {
-      let success = await fs.appendFile(url, data)
-      if (success === undefined) appendNext(dates.shift())
+async  function appendNext(url) {
+    if (url) {
+      try { 
+        let readStream = fsStream.createReadStream(url, "utf8");
+        readStream.on('data', async (chunk) => {
+          await fs.appendFile(destUrl,chunk)
+          appendNext(urlArray.shift())
+        })
+      } catch(error) {
+        console.log('Error to append:', error.message);
+      }
     }
-  }    
-      
+  }       
 }
-
-/*
-const stream = fs.createReadStream(url)
-stream.setEncoding('utf8');
-  stream.on('data', (chunk) => process.stdout.write(chunk));
-  stream.on('end', () => console.log('Reading finished successfully'))
-  stream.on('error', (error) => console.log('Error: ', error));
-*/
-
-
-
-
 
 
 
